d13aeafe38927d9e12e8a98b7b49081edd5e68d6
John Ojo <62852503+ojo-john@users.noreply.github.com>
2024-01-06 12:31:51 -0600

    First commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ojojoairlineauth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
